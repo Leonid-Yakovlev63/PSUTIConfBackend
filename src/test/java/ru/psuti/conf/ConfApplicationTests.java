52e31ffaeaf14738a36896c7b6b817e6e8ec6f69
Leonid-Yakovlev63 <95966082+Leonid-Yakovlev63@users.noreply.github.com>
2025-01-19 16:12:43 +0400

    Initial Commit

---
+++ src/test/java/ru/psuti/conf/ConfApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.psuti.conf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
